eb32fd11bf614abb75a781b9eb4fc92213b07873
lopamudrasatpathy <lopamudra.satpathy@gmail.com>
2020-11-16 16:47:31 -0600

    Initial version

---
+++ src/main/java/com/ridgewood/trapedge/TrapedgeApplication.java
@@ -0,0 +1,13 @@
+package com.ridgewood.trapedge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrapedgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrapedgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ridgewood/trapedge/TrapedgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ridgewood.trapedge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrapedgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
